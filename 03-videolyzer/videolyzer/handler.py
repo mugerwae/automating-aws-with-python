ea650e379a6379ce570364acd9a852c64ebf071b
Eric Mugerwa <eric.mugerwa@gigify.africa>
2022-07-03 09:57:40 +0300

    Get video label data from rekognition

--- 03-videolyzer/videolyzer/handler.py
+++ 03-videolyzer/videolyzer/handler.py
@@ -1,15 +1,18 @@
+import json
+
 import urllib
 
 import boto3
 
 import os
 
+
 def start_label_detection(bucket, key):
     """Label detection function """
     rekognition_client = boto3.client('rekognition')
     response = rekognition_client.start_label_detection(
         Video={
-            'S3Object': { 
+            'S3Object': {
                 'Bucket': bucket,
                 'Name': key
             }
@@ -22,18 +25,49 @@ def start_label_detection(bucket, key):
     print(response)
     return
 
+
+def get_video_labels(job_id):
+    rekognition_client = boto3.client('rekognition')
+
+    response = rekognition_client.get_label_detection(JobId=job_id)
+
+    next_token = response.get('NextToke', None)
+
+    while next_token:
+        next_page = rekognition_client.get_label_detection(JobId=job_id, NextToken=next_token)
+
+        next_token = next_page.get('NextToken', None)
+
+        response['Labels'].extend(next_page['Labels'])
+
+    return response
+
+
+def put_labels_in_db(data, video_name, video_bucket):
+    pass
+
+
 def start_processing_video(event, context):
     """video processing function"""
     for record in event['Records']:
         start_label_detection(
             record['s3']['bucket']['name'],
             urllib.parse.unquote_plus(record['s3']['object']['key'])
-            )
+        )
 
     return
 
+
 def handle_label_detection(event, context):
     """Label detection"""
-    print(event)
+    for record in event['Records']:
+        message = json.loads(record['Sns']['Message'])
+        job_id = message['JobId']
+        s3_object = message['Video']['S3ObjectName']
+        s3_bucket = message['Video']['S3Bucket']
+
+        response = get_video_labels(job_id)
+        print(response)
+        put_labels_in_db(response, s3_object, s3_bucket)
 
     return
