1d58225f8d4d0e9aa23288e037875a21d3598b73
Eric Mugerwa <eric.mugerwa@gigify.africa>
2022-07-03 10:47:09 +0300

    Save data from get_label_detection to dynamodb

--- 03-videolyzer/videolyzer/handler.py
+++ 03-videolyzer/videolyzer/handler.py
@@ -1,9 +1,6 @@
 import json
-
 import urllib
-
 import boto3
-
 import os
 
 
@@ -21,30 +18,42 @@ def start_label_detection(bucket, key):
             'SNSTopicArn': os.environ['REKOGNITION_SNS_TOPIC_ARN'],
             'RoleArn': os.environ['REKOGNITION_ROLE_ARN']
         })
-
-    print(response)
     return
 
 
 def get_video_labels(job_id):
     rekognition_client = boto3.client('rekognition')
-
     response = rekognition_client.get_label_detection(JobId=job_id)
-
     next_token = response.get('NextToke', None)
-
     while next_token:
         next_page = rekognition_client.get_label_detection(JobId=job_id, NextToken=next_token)
-
         next_token = next_page.get('NextToken', None)
-
         response['Labels'].extend(next_page['Labels'])
-
     return response
 
 
+def make_item(data):
+    if isinstance(data, dict):
+        return {k: make_item(v) for k, v in data.items()}
+    if isinstance(data, list):
+        return [make_item(v) for v in data]
+    if isinstance(data, float):
+        return str(data)
+    return data
+
+
 def put_labels_in_db(data, video_name, video_bucket):
-    pass
+    del data['ResponseMetadata']
+    del data['JobStatus']
+    data['videoName'] = video_name
+    data['videoBucket'] = video_bucket
+    dynamodb = boto3.resource('dynamodb')
+    table_name = os.environ['DYNAMODB_TABLE_NAME']
+    videos_table = dynamodb.Table(table_name)
+    data = make_item(data)
+    videos_table.put_item(Item=data)
+
+    return
 
 
 def start_processing_video(event, context):
@@ -65,9 +74,7 @@ def handle_label_detection(event, context):
         job_id = message['JobId']
         s3_object = message['Video']['S3ObjectName']
         s3_bucket = message['Video']['S3Bucket']
-
         response = get_video_labels(job_id)
-        print(response)
         put_labels_in_db(response, s3_object, s3_bucket)
 
     return
